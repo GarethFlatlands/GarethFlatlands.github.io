6384d10664a187599c648531da1804e072fb5016
GarethFlatlands <39499769+GarethFlatlands@users.noreply.github.com>
2018-05-22 21:09:57 +0100

    Update ghostPlayer.js

--- assets/js/ghostPlayer.js
+++ assets/js/ghostPlayer.js
@@ -1,8 +1,8 @@
 // Gets the audio and assigns the variable
-var slowAudio = new Audio("assets/sounds/theRest.mp3");
+var slowAudio = new Audio("assets/sounds/slowWalkers.mp3");
 
-// Slows the audion down 1 ir normal, 0.5 is half etc
-slowAudio.playbackRate = 0.5;
+// Slows the audion down 1 ir normal, 0.5 is half etc - not needed at this time
+//slowAudio.playbackRate = 0.5;
 
 // Plays the audion when the main page button is clicked
 $("#playButton").on("click", function() {
