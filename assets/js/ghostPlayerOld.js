cb7ce88d0f073c4959ad554d267f937a639d5e64
GarethFlatlands <39499769+GarethFlatlands@users.noreply.github.com>
2018-05-24 21:13:28 +0100

    jQuery Update and fadeout effect

--- assets/js/ghostPlayerOld.js
+++ assets/js/ghostPlayerOld.js
@@ -1,11 +1,10 @@
 // Gets the audio and assigns the variable
 var slowAudio = new Audio("assets/sounds/slowWalkers.mp3");
 
-// Slows the audion down 1 ir normal, 0.5 is half etc - not needed at this time
-//slowAudio.playbackRate = 0.5;
+// Slows the audion down 1 ir normal, 0.5 is half etc - not needed
+// slowAudio.playbackRate = 0.5;
 
 // Plays the audion when the main page button is clicked
 $("#playButton").on("click", function() {
 	slowAudio.play();
-	$("body").fadeOut(2010000);
 });
